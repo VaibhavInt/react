98a873c1f14c7a50327089dfb5b00b2e4f9603d6
Vaibhav <vaibhavshukla@gmail.com>
2022-04-09 13:59:01 +0530

    todo-REACT

---
+++ to-do/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import TodoForms from './Components/TodoForms';
+import TodoList from './Components/TodoList';
+import TodoForm from './Components/TodoForms';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList />
+
+    </div>
+  );
+}
+
+export default App;
